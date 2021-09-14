936bfe6d6e3f7a0db2a1bd2949ea5ebf34c45847
Flavio <flavio.cormerais@ynov.com>
2021-09-14 15:45:03 +0300

    hello

---
+++ printreversealphabet/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	var aRune string = "zyxwvutsrqponmlkjihgfedcba"
+	for i := 0; i < 26; i++ {
+		z01.PrintRune(rune(aRune[i]))
+	}
+	z01.PrintRune('\n')
+}

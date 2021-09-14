e98cf11d3da1950c687faaa75e6019476d5bb8b4
Flavio <flavio.cormerais@ynov.com>
2021-09-14 19:21:01 +0300

    hello

---
+++ printcomb.go
@@ -0,0 +1,27 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	chiffre1 := 0
+	chiffre2 := 0
+	chiffre3 := 0
+	for i := 0; i < 789; i++ {
+		chiffre1++
+		if chiffre1 == 10 {
+			chiffre1 = 0
+			chiffre2++
+		}
+		if chiffre2 == 10 {
+			chiffre3++
+			chiffre2 = 0
+		}
+		if chiffre1 != chiffre2 && chiffre2 != chiffre3 && chiffre3 != chiffre1 {
+			z01.PrintRune(rune(chiffre1))
+			z01.PrintRune(rune(chiffre2))
+			z01.PrintRune(rune(chiffre3))
+			z01.PrintRune(',')
+			z01.PrintRune(' ')
+		}
+	}
+}

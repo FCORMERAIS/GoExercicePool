68e67022966622058129e0c1d4dd698188247b2c
Flavio <flavio.cormerais@ynov.com>
2021-09-14 14:50:03 +0300

    hello

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,7 +1,11 @@
 package main
+
 import "github.com/01-edu/z01"
+
 func main(){
 	var aRune string = "abcdefghijklmnopqrstuvwxyz"
 	for i := 0; i < 26; i++{
-		z01.PrintRune(rune(aRune[i]))}
-	z01.PrintRune('\n')}
+		z01.PrintRune(rune(aRune[i]))
+	}
+	z01.PrintRune('\n')
+}

5311cab37ec1d112315faf6c4f25a47752116a74
Flavio <flavio.cormerais@ynov.com>
2021-09-14 16:50:10 +0300

    hello

--- isnegative/main.go
+++ isnegative/main.go
@@ -5,10 +5,10 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	if nb >= 0 {
 		z01.PrintRune('F')
-		z01.PrintRune('\n')	
-	}  
-	
-	if nb < 0{
+		z01.PrintRune('\n')
+	}
+
+	if nb < 0 {
 		z01.PrintRune('F')
 		z01.PrintRune('\n')
 	}

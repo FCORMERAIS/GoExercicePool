97cfeaae49fdc694f80af01fd8d2030b85cde0bc
Flavio <flavio.cormerais@ynov.com>
2021-09-15 16:46:01 +0300

    hello

--- printcomb.go
+++ printcomb.go
@@ -8,7 +8,7 @@ func PrintComb() {
 	chiffre3 := '0'
 	for i := 0; i < 789; i++ {
 		chiffre1++
-		if chiffre1 != chiffre2 && chiffre2 != chiffre3 && chiffre3 != chiffre1 && chiffre3 > chiffre2 && chiffre2 > chiffre1 {
+		if chiffre1 != chiffre2 && chiffre2 != chiffre3 && chiffre3 != chiffre1 && chiffre3 < chiffre2 && chiffre2 < chiffre1 {
 			z01.PrintRune(rune(chiffre3))
 			z01.PrintRune(rune(chiffre2))
 			z01.PrintRune(rune(chiffre1))

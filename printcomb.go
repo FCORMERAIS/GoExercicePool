42609f59418959188b130293cab4d047d4f0e9da
Flavio <flavio.cormerais@ynov.com>
2021-09-15 16:41:09 +0300

    hello

--- printcomb.go
+++ printcomb.go
@@ -7,6 +7,7 @@ func PrintComb() {
 	chiffre2 := '0'
 	chiffre3 := '0'
 	for i := 0; i < 789; i++ {
+		chiffre1++
 		if chiffre1 != chiffre2 && chiffre2 != chiffre3 && chiffre3 != chiffre1 && chiffre3 > chiffre2 && chiffre2 > chiffre1 {
 			z01.PrintRune(rune(chiffre1))
 			z01.PrintRune(rune(chiffre2))
@@ -14,7 +15,6 @@ func PrintComb() {
 			z01.PrintRune(',')
 			z01.PrintRune(' ')
 		}
-		chiffre1++
 		if chiffre1 == '9' {
 			chiffre1 = '0'
 			chiffre2++

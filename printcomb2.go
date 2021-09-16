6c640a10bc2f5037f0a3d6ab7d5f46a1de4a7315
Flavio <flavio.cormerais@ynov.com>
2021-09-16 14:04:20 +0300

    hello

--- printcomb2.go
+++ printcomb2.go
@@ -7,26 +7,47 @@ func PrintComb2() {
 	rune2 := '0'
 	rune3 := '0'
 	rune4 := '0'
-	for i := 0; i < 100; i++ {
-		for k := 0; k < 100; k++ {
-			rune4++
+	for rune1 != '9' || rune2 != '8' {
+		rune4++
+		if rune1 < rune3  {
 			z01.PrintRune(rune(rune1))
 			z01.PrintRune(rune(rune2))
 			z01.PrintRune(rune(' '))
 			z01.PrintRune(rune(rune3))
 			z01.PrintRune(rune(rune4))
-			if rune4 == 9 {
-				rune4 = 0
-				rune3++
-			}
-			if rune3 == 9 {
-				rune2++
-				rune3 = 0
-			}
-			if rune2 == 9 {
-				rune1++
-				rune2 = 0
+			z01.PrintRune(rune(','))
+			z01.PrintRune(rune(' '))
+		} else if rune1 == rune3 {
+			if rune2 < rune4 {
+				z01.PrintRune(rune(rune1))
+				z01.PrintRune(rune(rune2))
+				z01.PrintRune(rune(' '))
+				z01.PrintRune(rune(rune3))
+				z01.PrintRune(rune(rune4))
+				z01.PrintRune(rune(','))
+				z01.PrintRune(rune(' '))
 			}
 		}
+		if rune2 == '9'&& rune3 == '9' && rune4 =='9' {
+			rune1++
+			rune2 = '0'	
+			rune3 = '0'
+			rune4 = '0'			
+		}
+		if rune3 == '9' && rune4 == '9' {
+			rune2++
+			rune3 = '0'
+			rune4 = '0'
+		}
+		if rune4 == '9' {
+			rune4 = '0'
+			rune3++
+		}
 	}
+	z01.PrintRune(rune(rune1))
+	z01.PrintRune(rune(rune2))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('9'))
+	z01.PrintRune(rune('9'))
+	z01.PrintRune(rune('\n'))
 }

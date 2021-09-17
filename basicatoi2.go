506fa5922ae6992ba47ec6102586d1057b517376
Flavio <flavio.cormerais@ynov.com>
2021-09-17 16:33:08 +0300

    hello

--- basicatoi2.go
+++ basicatoi2.go
@@ -6,7 +6,7 @@ func BasicAtoi2(s string) int {
 	for i := len(s) - 1; i >= 0; i-- {
 		intA = intA + int(s[i]%48)*puissanceDix
 		puissanceDix = puissanceDix * 10
-		if 48 >= int(s[i]) || int(s[i]) > 58 {
+		if 48 > int(s[i]) || int(s[i]) >= 58 {
 			return 0
 		}
 	}

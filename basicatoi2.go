e466f6258bbf4571a7b7e39488678057a6893f2e
Flavio <flavio.cormerais@ynov.com>
2021-09-17 16:31:21 +0300

    hello

---
+++ basicatoi2.go
@@ -0,0 +1,14 @@
+package piscine
+
+func BasicAtoi2(s string) int {
+	puissanceDix := 1
+	intA := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		intA = intA + int(s[i]%48)*puissanceDix
+		puissanceDix = puissanceDix * 10
+		if 48 >= int(s[i]) || int(s[i]) > 58 {
+			return 0
+		}
+	}
+	return intA
+}

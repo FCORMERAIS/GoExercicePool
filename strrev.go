da65316ae4c09e7f863070ae2231c1601803c8fe
Flavio <flavio.cormerais@ynov.com>
2021-09-17 12:14:43 +0300

    hello

--- strrev.go
+++ strrev.go
@@ -2,9 +2,7 @@ package piscine
 
 func StrRev(s string) string {
 	result := ""
-	//letter := ' '
 	for i := len(s); i == 0; i-- {
-		//letter = rune(s[i])
 		result = result + "1"
 	}
 	return result

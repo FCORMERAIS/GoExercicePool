adb2c2ceb0afbea846b5c3be9c3ed9279370c2d6
Flavio <flavio.cormerais@ynov.com>
2021-09-17 23:34:50 +0300

    hello

--- sortintegertable.go
+++ sortintegertable.go
@@ -2,7 +2,7 @@ package piscine
 
 func SortIntegerTable(table []int) {
 	index := 0
-	for i := -1; i < len(table)-1; i++ {
+	for i := 0; i < len(table)-1; i++ {
 		minimum := 1000000
 		for k := i; k < len(table)-1; k++ {
 			if table[k] < minimum {

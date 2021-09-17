b2fa2922e49ab937d7183ce9f3775ac4a71c37fa
Flavio <flavio.cormerais@ynov.com>
2021-09-17 15:42:13 +0300

    hello

---
+++ basicatoi.go
@@ -0,0 +1,11 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	puissanceDix := 1
+	intA := 0
+	for i := len(s) - 1; i <= 0; i-- {
+		intA = intA + ((int(rune(s[i])))%48)*puissanceDix
+		puissanceDix = puissanceDix * 10
+	}
+	return intA
+}

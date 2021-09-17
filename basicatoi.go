8abd392f8da5be59b08d8313fc7216b303c058c7
Flavio <flavio.cormerais@ynov.com>
2021-09-17 15:49:46 +0300

    hello

--- basicatoi.go
+++ basicatoi.go
@@ -4,7 +4,7 @@ func BasicAtoi(s string) int {
 	puissanceDix := 1
 	intA := 0
 	for i := len(s) - 1; i <= 0; i-- {
-		intA = intA + ((int(rune(s[i])))%48)*puissanceDix
+		intA = intA + int(s[i]%48)*puissanceDix
 		puissanceDix = puissanceDix * 10
 	}
 	return intA

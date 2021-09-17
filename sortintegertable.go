ca080a25cfcc89af15d4541ebe498058f05f3153
Flavio <flavio.cormerais@ynov.com>
2021-09-17 23:35:26 +0300

    hello

--- sortintegertable.go
+++ sortintegertable.go
@@ -4,7 +4,7 @@ func SortIntegerTable(table []int) {
 	index := 0
 	for i := 0; i < len(table)-1; i++ {
 		minimum := 1000000
-		for k := i; k < len(table)-1; k++ {
+		for k := i; k < len(table); k++ {
 			if table[k] < minimum {
 				minimum = table[k]
 				index = k

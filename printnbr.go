238706e8e3a1bc20f67cc74cebd985cb58766783
Flavio <flavio.cormerais@ynov.com>
2021-09-20 10:49:27 +0300

    hello

--- printnbr.go
+++ printnbr.go
@@ -3,11 +3,15 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	liste := make([]int, 1)
+	liste :=  []int{}
 	nb := 0
-	for n/10 == 0 {
+	if n <0 {
+		z01.PrintRune(rune('-'))
+	}
+	for n/10 == 0{
 		nb = n % 10
-		liste = append(liste, nb)
+		liste = append(nb,liste)
+		n = n/10 
 	}
 	for i := 0; i < len(liste); i++ {
 		z01.PrintRune(rune(liste[len(liste)-1]))

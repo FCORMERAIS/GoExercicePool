0fd6988c535a5e28654be43aa825d794a6326f04
Flavio <flavio.cormerais@ynov.com>
2021-09-21 17:44:55 +0300

    hello

--- findnextprime.go
+++ findnextprime.go
@@ -1,15 +1,15 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-    if nb < 1 {
-        return 2
-    }
-    for i := nb; i < 9223372036854775807; i++ {
-        for a := 2; a < i; a++ {
-            if i%a != 0 {
-                return i
-            }
-        }
-    }
-    return 0
+	if nb < 1 {
+		return 2
+	}
+	for i := nb; i < 9223372036854775807; i++ {
+		for a := 2; a < i; a++ {
+			if i%a != 0 {
+				return i
+			}
+		}
+	}
+	return 0
 }

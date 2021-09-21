986b6d631e17234d10f6901c11994d4fbd609387
Flavio <flavio.cormerais@ynov.com>
2021-09-21 15:30:35 +0300

    hello

--- iterativepower.go
+++ iterativepower.go
@@ -4,9 +4,8 @@ func IterativePower(nb int, power int) int {
 	if power < 0 || power > 50 {
 		return 0
 	}
-	result := nb
 	for i := 0; i < power; i++ {
-		result = result + (nb * power)
+		nb = nb * power
 	}
-	return result
+	return nb
 }

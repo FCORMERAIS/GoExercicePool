f6278db5eb87015285e161dfa80813f23ce184cc
Flavio <flavio.cormerais@ynov.com>
2021-09-21 22:31:56 +0300

    hello

--- findnextprime.go
+++ findnextprime.go
@@ -4,9 +4,6 @@ func FindNextPrime(nb int) int {
 	if nb < 3 {
 		return 2
 	}
-	if nb == 4 {
-		return 5
-	}
 	for i := nb; i < 9223372036854775807; i++ {
 		pas_bon := 0
 		if nb < 14 {
@@ -16,7 +13,7 @@ func FindNextPrime(nb int) int {
 				}
 			}
 			if pas_bon == 0 {
-				return i
+				return i + 1
 			}
 		}
 		if i%2 == 0 || i%3 == 0 || i%5 == 0 || i%7 == 0 || i%11 == 0 || i%13 == 0 {

781ba4e5da4fd7e697093073cd0154f8d08c16ac
Flavio <flavio.cormerais@ynov.com>
2021-09-21 14:32:47 +0300

    hello

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,11 +1,11 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb < 0 || nb > 15 {
+	if nb < 0 || nb > 10 {
 		return 0
 	}
 	result := 1
-	for i := 1; i > nb; i++ {
+	for i := 1; i < nb; i++ {
 		result = result * i
 	}
 	return result

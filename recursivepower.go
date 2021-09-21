71da753acd4da11ebd6e122df395fe306a798569
Flavio <flavio.cormerais@ynov.com>
2021-09-21 16:01:10 +0300

    hello

--- recursivepower.go
+++ recursivepower.go
@@ -4,6 +4,9 @@ func RecursivePower(nb int, power int) int {
 	if power == 0 {
 		return 1
 	}
+	if power == 1 {
+		return nb
+	}
 	if power > 1 && power < 50 {
 		return nb * RecursivePower(nb, power-1)
 	}

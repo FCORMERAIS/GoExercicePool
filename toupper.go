8e949c8f110cf7037da336fe269d3e3bf89a034b
Flavio <flavio.cormerais@ynov.com>
2021-09-22 15:59:13 +0300

    hello

--- toupper.go
+++ toupper.go
@@ -3,7 +3,7 @@ package piscine
 func ToUpper(s string) []rune {
 	sentence := []rune(s)
 	for i := 0; i < len(sentence); i++ {
-		if int(sentence[i]) <= 'z' && int(sentence[i]) >= 'a' {
+		if int(sentence[i]) <= 122 && int(sentence[i]) >= 97 {
 			sentence[i] = rune(int(sentence[i]) - 32)
 		}
 	}

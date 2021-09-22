217921d659e4e12fe3043d0b12a9a19e810473c4
Flavio <flavio.cormerais@ynov.com>
2021-09-22 12:23:09 +0300

    hello

--- nrune.go
+++ nrune.go
@@ -3,7 +3,10 @@ package piscine
 func NRune(s string, n int) rune {
 	if n < len(s) && n > 0 {
 		sentence := []rune(s)
-		return rune(sentence[n-2])
+		if rune(sentence[n-1]) == '!' {
+			return '!'
+		}
+		return rune(sentence[n-1])
 	}
 	return 0
 }

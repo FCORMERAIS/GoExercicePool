92e9781c0a1aeabeb9ee809feceb6740a714fc04
Flavio <flavio.cormerais@ynov.com>
2021-09-22 17:33:45 +0300

    hello

--- capitalize.go
+++ capitalize.go
@@ -9,7 +9,7 @@ func Capitalize(s string) string {
 	str = str + string(sentence[0])
 	for i := 1; i < len(sentence); i++ {
 		if sentence[i] >= 'a' && sentence[i] <= 'z' {
-			if sentence[i-1] >= 'a' && sentence[i-1] <= 'z' || sentence[i-1] >= '0' && sentence[i-1] <= '9' {
+			if sentence[i-1] >= 'a' && sentence[i-1] <= 'z' || sentence[i-1] >= '0' && sentence[i-1] <= '9' || sentence[i-1] >= 'A' && sentence[i-1] <= 'Z' {
 				sentence[i] = sentence[i] + 0
 			} else {
 				sentence[i] = sentence[i] - 32

6b490d4ef03edb968413feb4cb906e6d6634ac43
Flavio <flavio.cormerais@ynov.com>
2021-09-22 14:08:32 +0300

    hello

--- alphacount.go
+++ alphacount.go
@@ -4,7 +4,7 @@ func AlphaCount(s string) int {
 	counter := 0
 	sentence := []rune(s)
 	for i := 0; i < len(s); i++ {
-		if int(sentence[i]) <= 97 && int(sentence[i]) >= 65 {
+		if int(sentence[i]) <= 90 && int(sentence[i]) >= 65 {
 			counter++
 		}
 		if int(sentence[i]) <= 122 && int(sentence[i]) >= 97 {

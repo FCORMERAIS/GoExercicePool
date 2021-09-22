bfd85552de9d9ca54fb9059119b5e76aeb5689bb
Flavio <flavio.cormerais@ynov.com>
2021-09-22 12:56:42 +0300

    hello

--- compare.go
+++ compare.go
@@ -1,13 +1,13 @@
 package piscine
 
 func Compare(a, b string) int {
-	if len(a) == len(b) {
+	if a== b {
 		return 0
 	}
-	if len(a) < len(b) {
+	if a < b {
 		return -1
 	}
-	if len(a) > len(b) {
+	if a > b {
 		return 1
 	}
 	return 0

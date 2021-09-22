3424e47908e56eb202e4e901086f3dee6fb59e83
Flavio <flavio.cormerais@ynov.com>
2021-09-22 14:22:47 +0300

    hello

---
+++ index.go
@@ -0,0 +1,20 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	sentence := []rune(s)
+	motif := []rune(toFind)
+	counter_motif := 0
+	counter := 0
+	for i := 0; i < len(sentence)-len(motif); i++ {
+		counter = 0
+		for k := 0; k < len(motif); k++ {
+			if motif[k] == sentence[i+k] {
+				counter++
+			}
+		}
+		if counter == len(motif) {
+			counter_motif++
+		}
+	}
+	return counter_motif
+}

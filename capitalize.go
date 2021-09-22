685b775167481beaaf572876a88bf2319a4ab7bc
Flavio <flavio.cormerais@ynov.com>
2021-09-22 17:28:46 +0300

    hello

--- capitalize.go
+++ capitalize.go
@@ -4,13 +4,18 @@ func Capitalize(s string) string {
 	sentence := []rune(s)
 	str := ""
 	for i := 0; i < len(sentence); i++ {
-		if sentence[i] < 48 || sentence[i] > 57 {
-			if sentence[i] < 65 || sentence[i] > 90 {
-				if sentence[i] < 97 || sentence[i] > 122 {
-					if sentence[i+1] < 97 || sentence[i+1] > 122 {
-						sentence[i+1] = sentence[i+1] - 32
-					}
-				}
+		if sentence[i] >= 'a' && sentence[i] <= 'z' {
+			if sentence[i-1] >= 'a' && sentence[i-1] <= 'z' || sentence[i-1] >= '0' && sentence[i-1] <= '9' {
+				sentence[i] = sentence[i] + 0
+			} else {
+				sentence[i] = sentence[i] - 32
+			}
+		}
+		if sentence[i] >= 'A' && sentence[i] <= 'Z' {
+			if sentence[i-1] >= 'a' && sentence[i-1] <= 'z' || sentence[i-1] >= '0' && sentence[i-1] <= '9' {
+				sentence[i] = sentence[i] + 32
+			} else {
+				sentence[i] = sentence[i] + 0
 			}
 		}
 		str = str + string(sentence[i])

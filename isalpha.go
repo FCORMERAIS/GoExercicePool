76d2530899c3eb3032ebe964ca749e9126b0e7c7
Flavio <flavio.cormerais@ynov.com>
2021-09-22 15:33:08 +0300

    hello

--- isalpha.go
+++ isalpha.go
@@ -6,7 +6,7 @@ func IsAlpha(s string) bool {
 	if len(s) == 0 {
 		return true
 	}
-	for i := 0; i < len(s); i++ {
+	for i := 0; i < len(sentence); i++ {
 		if int(sentence[i]) <= 90 && int(sentence[i]) >= 65 {
 			counter++
 		}

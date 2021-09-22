2e69898bd96725e87ccd7f88b3be9238d8232244
Flavio <flavio.cormerais@ynov.com>
2021-09-22 09:52:27 +0300

    hello

--- findnextprime.go
+++ findnextprime.go
@@ -1,7 +1,6 @@
 package piscine
 
 func FindNextPrime(nb int) int {
-	c := 0
 	if nb < 3 {
 		return 2
 	}
@@ -20,36 +19,21 @@ func FindNextPrime(nb int) int {
 	if nb == 12 || nb == 13 {
 		return 13
 	}
-	if nb == 14 || nb == 15 || nb == 16 || nb == 17 {
-		return 17
-	}
-	if nb == 18 || nb == 19 {
-		return 19
-	}
-	if nb == 20 || nb == 21 || nb == 22 || nb == 23 {
-		return 23
-	}
-	if nb == 24 || nb == 25 || nb == 26 || nb == 27 || nb == 28 || nb == 29 {
-		return 29
-	}
 	for i := nb; i < 9223372036854775807; i++ {
 		pas_bon := 0
-		if i%2 == 0 || i%3 == 0 || i%5 == 0 || i%7 == 0 || i%11 == 0 || i%13 == 0 || i%17 == 0 || i%19 == 0 || i%23 == 0 || i%29 == 0 {
+		if i%2 == 0 || i%3 == 0 || i%5 == 0 || i%7 == 0 || i%11 == 0 || i%13 == 0 {
 			pas_bon = 1
 		} else {
-			for a := 2; a < i; a++ {
-				if a%2 == 0 {
-					c = 0
-				} else {
-					if i%a == 0 {
-						pas_bon = 1
-					}
+			for a := 13; a < i; a++ {
+				if i%a == 0 {
+					pas_bon = 1
 				}
+				a++
 			}
 		}
 		if pas_bon == 0 {
 			return i
 		}
 	}
-	return c
+	return 0
 }

c9c629cf8880d3ae6491397fbe56b90affbcd117
Flavio <flavio.cormerais@ynov.com>
2021-09-22 17:40:39 +0300

    hello

--- join.go
+++ join.go
@@ -4,7 +4,9 @@ func Join(strs []string, sep string) string {
 	str := ""
 	for i := 0; i < len(strs); i++ {
 		str = str + string(strs[i])
-		str = str + string(sep)
+		if i+1 != len(strs) {
+			str = str + sep
+		}
 	}
 	return str
 }

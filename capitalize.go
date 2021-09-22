e70b9bde41b3fa6a1bf467e2b8e9b3c6456b5db6
Flavio <flavio.cormerais@ynov.com>
2021-09-22 17:31:27 +0300

    hello

--- capitalize.go
+++ capitalize.go
@@ -6,6 +6,7 @@ func Capitalize(s string) string {
 	if sentence[0] >= 'a' && sentence[0] <= 'z' {
 		sentence[0] = sentence[0] - 32
 	}
+	str = str + string(sentence[0])
 	for i := 1; i < len(sentence); i++ {
 		if sentence[i] >= 'a' && sentence[i] <= 'z' {
 			if sentence[i-1] >= 'a' && sentence[i-1] <= 'z' || sentence[i-1] >= '0' && sentence[i-1] <= '9' {

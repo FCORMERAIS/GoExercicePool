d3448a69af9c9cc15f6cef4eafe112ad74e4e601
Flavio <flavio.cormerais@ynov.com>
2021-09-22 14:05:39 +0300

    hello

---
+++ alphacount.go
@@ -0,0 +1,15 @@
+package piscine
+
+func AlphaCount(s string) int {
+	counter := 0
+	sentence := []rune(s)
+	for i := 0; i < len(s); i++ {
+		if int(sentence[i]) <= 97 && int(sentence[i]) >= 65 {
+			counter++
+		}
+		if int(sentence[i]) <= 122 && int(sentence[i]) >= 97 {
+			counter++
+		}
+	}
+	return counter
+}

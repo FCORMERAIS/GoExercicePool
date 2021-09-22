fc3e8af262bac40527dfd6769ac7831ce85c0d46
Flavio <flavio.cormerais@ynov.com>
2021-09-22 15:24:31 +0300

    hello

---
+++ isalpha.go
@@ -0,0 +1,22 @@
+package piscine
+
+func IsAlpha(s string) bool {
+	counter := 0
+	sentence := []rune(s)
+	if len(s) == 0 {
+		return true
+	}
+	for i := 0; i < len(s); i++ {
+		if int(sentence[i]) <= 90 && int(sentence[i]) >= 65 {
+			counter++
+		}
+		if int(sentence[i]) <= 122 && int(sentence[i]) >= 97 {
+			counter++
+		}
+	}
+	if counter == len(s) {
+		return true
+	} else {
+		return false
+	}
+}

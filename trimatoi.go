26ab817555bd11acba6e9531719fb9a46592774b
Flavio <flavio.cormerais@ynov.com>
2021-09-23 09:58:57 +0300

    hello

---
+++ trimatoi.go
@@ -0,0 +1,25 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func TrimAtoi(s string) int {
+	puissanceDix := 1
+	intA := 0
+	negative := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		if int(s[i]) == 45 && i == 0 {
+			negative += 1
+		} else if 48 > int(s[i]) || int(s[i]) >= 58 {
+			return 0
+		} else {
+			intA = intA + int(s[i]%48)*puissanceDix
+			puissanceDix = puissanceDix * 10
+		}
+	}
+	if negative == 1 {
+		intA = -intA
+		return intA
+	} else {
+		return intA
+	}
+}

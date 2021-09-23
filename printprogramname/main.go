50314d75a5b338a778653879ef2519a990cb18d4
Flavio <flavio.cormerais@ynov.com>
2021-09-23 12:33:47 +0300

    hello

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -5,14 +5,14 @@ import (
 
 	"github.com/01-edu/z01"
 )
-func PrintName(s string) string{
+
+func PrintName(s string) {
 	for i := 0; i < len(s); i++ {
-		if s[i] != '.' && s[i] != '/'{
+		if s[i] != '.' && s[i] != '/' {
 			z01.PrintRune(rune(s[i]))
 		}
 	}
 }
-
 
 func main() {
 	argument := os.Args

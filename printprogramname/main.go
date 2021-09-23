92f54ee996568ef63ab1df87e0df3433c285d7ed
Flavio <flavio.cormerais@ynov.com>
2021-09-23 12:12:36 +0300

    hello

---
+++ printprogramname/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/01-edu/z01"
+import "os"
+
+func main() {
+	argument := os.Args
+	text := argument[0]
+	for i := 0; i < len(argument[0]); i++ {
+		z01.PrintRune(rune(text[i]))
+	}
+}

eb9415e8ffc0abe0750892ce64f7a2eb614d694a
Flavio <flavio.cormerais@ynov.com>
2021-09-23 16:12:12 +0300

    hello

--- sortparams/main.go
+++ sortparams/main.go
@@ -6,10 +6,13 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func SortIntegerTable(table []int) []int {
+func SortIntegerTable(table []string) []string {
 	index := 0
+	if len(table) == 1 {
+		return table
+	}
 	for i := 0; i < len(table)-1; i++ {
-		minimum := 1000000
+		minimum := table[0]
 		for k := i; k < len(table); k++ {
 			if table[k] < minimum {
 				minimum = table[k]
@@ -21,20 +24,17 @@ func SortIntegerTable(table []int) []int {
 	return table
 }
 
-func FirstRune(s string) rune {
-	sentence := []rune(s)
-	return rune(sentence[0])
-}
-
 func main() {
 	argument := os.Args
-	list := []int{}
+	list := []string{}
 	for i := 1; i < len(argument); i++ {
-		list = append(list, int(FirstRune(argument[i])))
+		list = append(list, argument[i])
 	}
 	list = SortIntegerTable(list)
 	for i := 1; i < len(argument); i++ {
-		z01.PrintRune(rune(list[i]))
+		for k := 0; k < len(list[i]); k++ {
+			z01.PrintRune(rune(list[i][k]))
+		}
 		z01.PrintRune(rune('\n'))
 	}
 }

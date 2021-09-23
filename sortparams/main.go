8bea06fbd8b0fc9e0b6c7a0c5b855758d0cf9227
Flavio <flavio.cormerais@ynov.com>
2021-09-23 16:31:06 +0300

    hello

--- sortparams/main.go
+++ sortparams/main.go
@@ -12,7 +12,7 @@ func SortIntegerTable(table []string) []string {
 		return table
 	}
 	for i := 0; i <= len(table)-1; i++ {
-		minimum := table[0]
+		minimum := table[i]
 		for k := i; k < len(table); k++ {
 			if table[k] < minimum {
 				minimum = table[k]

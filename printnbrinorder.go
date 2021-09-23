761e5def2f262839947ffe6e41c02ea940f07dbb
Flavio <flavio.cormerais@ynov.com>
2021-09-23 09:18:29 +0300

    hello

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -2,7 +2,7 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintNbrInOrder(n int) []int {
+func PrintNbrInOrder(n int) {
 	liste := []int{}
 	nb := 0
 	for k := 0; 0 == n/10; k++ {
@@ -10,20 +10,20 @@ func PrintNbrInOrder(n int) []int {
 		liste = append(liste, nb)
 		n = n / 10
 	}
-	return liste
-	index := 0
-	for i := 0; i < len(liste)-1; i++ {
-		minimum := 1000000
-		for k := i; k < len(liste); k++ {
-			if liste[k] < minimum {
-				minimum = liste[k]
-				index = k
-			}
-		}
-		liste[i], liste[index] = liste[index], liste[i]
-	}
 	for i := 0; i < len(liste); i++ {
 		z01.PrintRune(rune(liste[i]))
+		index := 0
+		for i := 0; i < len(liste)-1; i++ {
+			minimum := 1000000
+			for k := i; k < len(liste); k++ {
+				if liste[k] < minimum {
+					minimum = liste[k]
+					index = k
+				}
+			}
+			liste[i], liste[index] = liste[index], liste[i]
+		}
+		//for i := 0; i < len(liste); i++ {
+		//	z01.PrintRune(rune(liste[i]))
 	}
-	return liste
 }

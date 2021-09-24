49e451c23e24afd35cd74dab4bfb130e62bf8e99
Flavio <flavio.cormerais@ynov.com>
2021-09-24 14:59:16 +0300

    hello

---
+++ split.go
@@ -0,0 +1,33 @@
+package piscine
+
+func Split(s, sep string) []string {
+	compteur := 0
+	str := ""
+	var list1 []string
+	for i := 0; i < len(s); i++ {
+		if s[i] != sep[0] {
+			compteur++
+		} else if s[i] == sep[0] && compteur != 0 {
+			compteur_sep := 0
+			for j := 0; j < len(sep); j++ {
+				if len(s)-i >= len(sep) && s[i+j] == sep[j] {
+					compteur_sep++
+				}
+			}
+			if compteur_sep == len(sep) {
+				for k := 0; k < compteur; k++ {
+					str = str + string(s[i-compteur+k])
+				}
+				list1 = append(list1, str)
+				str = ""
+				compteur = 0
+			}
+		}
+	}
+	for i := compteur; i > 0; i-- {
+		compteur--
+		str = str + string(s[len(s)-1-compteur])
+	}
+	list1 = append(list1, str)
+	return list1
+}

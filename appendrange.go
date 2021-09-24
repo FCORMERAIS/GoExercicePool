c2f570e18fec079b5c971edaa3ddcaa8581e15bd
Flavio <flavio.cormerais@ynov.com>
2021-09-24 11:42:45 +0300

    hello

--- appendrange.go
+++ appendrange.go
@@ -4,8 +4,11 @@ func AppendRange(min, max int) []int {
 	var answer []int
 	if min >= max {
 		return answer
+	} else if min+1 == max {
+		answer = append(answer, min)
+		return answer
 	} else {
-		for i := max - min; i < max; i++ {
+		for i := max - min - 1; i < max; i++ {
 			answer = append(answer, i)
 		}
 		return answer

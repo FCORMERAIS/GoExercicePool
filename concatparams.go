b0cbe767ad4be2057890e7560c9f925199f7ad62
Flavio <flavio.cormerais@ynov.com>
2021-09-24 12:14:43 +0300

    hello

--- concatparams.go
+++ concatparams.go
@@ -2,10 +2,10 @@ package piscine
 
 func ConcatParams(args []string) string {
 	list := ""
-	for i := 0; i < len(args); i++ {
+	for i := 0; i < len(args)-1; i++ {
 		list = list + args[i]
 		list = list + "\n"
 	}
-	list = list + "\n"
+	list = list + args[len(args)-1]
 	return list
 }

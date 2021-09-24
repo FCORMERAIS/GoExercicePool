edf071fe5a0ff5e9f2f82e5d39c1ed2aeba942fe
Flavio <flavio.cormerais@ynov.com>
2021-09-24 11:44:44 +0300

    hello

--- appendrange.go
+++ appendrange.go
@@ -2,15 +2,8 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	var answer []int
-	if min >= max {
-		return answer
-	} else if min+1 == max {
-		answer = append(answer, min)
-		return answer
-	} else {
-		for i := max - min - 1; i < max; i++ {
-			answer = append(answer, i)
-		}
-		return answer
+	for i := min; i < max; i++ {
+		answer = append(answer,i)
 	}
+	return answer
 }

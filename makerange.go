72d630dd5004979829adfa66a93ab3d6eed3919e
Flavio <flavio.cormerais@ynov.com>
2021-09-24 11:54:35 +0300

    hello

--- makerange.go
+++ makerange.go
@@ -1,7 +1,11 @@
 package piscine
 
 func MakeRange(min, max int) []int {
+	pas_bon := []int{}
 	answer := make([]int, max-min)
+	if max-min < 0 {
+		return pas_bon
+	}
 	for i := min; i < max; i++ {
 		answer[i] = i + 1
 	}

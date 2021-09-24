44e32530b07c0738e7785542db8584fff8bc13d6
Flavio <flavio.cormerais@ynov.com>
2021-09-24 16:37:13 +0300

    hello

--- split.go
+++ split.go
@@ -1,37 +1,30 @@
 package piscine
 
 func Split(s, sep string) []string {
-	compteur := 0
-	str := ""
-	var list1 []string
-	if s[0] == 'H' && s[1] == 'e' && s[2] == 'l' {
-		str = "H"
-	}
-	for i := 0; i < len(s); i++ {
-		if s[i] != sep[0] {
-			compteur++
-		} else if s[i] == sep[0] && compteur != 0 {
-			compteur_sep := 0
-			for j := 0; j < len(sep); j++ {
-				if len(s)-i >= len(sep) && s[i+j] == sep[j] {
-					compteur_sep++
-				}
-			}
-			if compteur_sep == len(sep) {
-				for k := 0; k < compteur; k++ {
-					str = str + string(s[i-compteur+k])
-				}
-				list1 = append(list1, str)
-				str = ""
-				compteur = 0
-				i = i + len(sep) - 1
-			}
-		}
-	}
-	for i := compteur; i > 0; i-- {
-		compteur--
-		str = str + string(s[len(s)-1-compteur])
-	}
-	list1 = append(list1, str)
-	return list1
+    var list1 []string
+    word := ""
+    testsep := ""
+    for i := 0; i < len(s); i++ {
+        if i+len(sep) < len(s) {
+            for k := i; k < i+len(sep); k++ {
+                testsep = testsep + string(s[k])
+            }
+        }
+        if i == len(s)-1 {
+            word = word + string(s[i])
+            list1 = append(list1, word)
+            word = ""
+        } else if testsep == sep {
+            list1 = append(list1, word)
+            word = ""
+            i = i + len(sep) - 1
+        } else {
+            if string(s[i]) == " " {
+            } else {
+                word = word + string(s[i])
+            }
+        }
+        testsep = ""
+    }
+    return list1
 }

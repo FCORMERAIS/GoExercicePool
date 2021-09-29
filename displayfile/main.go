d1465fe88369e4ca168435f6e8d011b9cd0749ae
Flavio <flavio.cormerais@ynov.com>
2021-09-29 15:18:05 +0300

    hello

---
+++ displayfile/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("quest8.txt")
+
+	if err != nil {
+		fmt.Printf("error : ", err.Error())
+	}
+	fmt.Println(file.Stat())
+	arr := make([]byte, 6)
+	file.Read(arr)
+	fmt.Println(string(arr))
+	file.Close()
+}

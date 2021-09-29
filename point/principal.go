97245c49ed7f8cdc5e690f8a544d2a98c7d5ffbf
Flavio <flavio.cormerais@ynov.com>
2021-09-29 12:41:28 +0300

    hello

---
+++ point/principal.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+type point struct {
+	x1 rune
+	x2 rune
+	y1 rune
+	y2 rune
+}
+
+func setPoint(ptr *point) {
+	ptr.x1 = '4'
+	ptr.x2 = '2'
+	ptr.y1 = '2'
+	ptr.y2 = '1'
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+	z01.PrintRune(rune('x'))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('='))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune(points.x1))
+	z01.PrintRune(rune(points.x2))
+	z01.PrintRune(rune(','))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('y'))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune('='))
+	z01.PrintRune(rune(' '))
+	z01.PrintRune(rune(points.y1))
+	z01.PrintRune(rune(points.y2))
+
+}

ae3f1cd0fb0c0de5fe265c18d740fe74a07e4679
Flavio <flavio.cormerais@ynov.com>
2021-09-29 15:37:09 +0300

    hello

--- displayfile/main.go
+++ displayfile/main.go
@@ -6,10 +6,8 @@ import (
 )
 
 func main() {
-	file, err := os.Open("quest.txt")
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
+	file, err := os.Open("quest8.txt")
+	fmt.Printf(err.Error())
 	arr := make([]byte, 14)
 	file.Read(arr)
 	fmt.Println(string(arr))

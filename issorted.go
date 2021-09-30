d9849b14959b7c5dc57e40bbbd87ddea806f4643
Flavio <flavio.cormerais@ynov.com>
2021-09-30 14:31:17 +0300

    hello

--- issorted.go
+++ issorted.go
@@ -2,13 +2,25 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	var result []int
+	counter == 0
 	for i := 0; i < len(a)-1; i++ {
 		result = append(result, f(a[i], a[i+1]))
 	}
 	for i := 0; i < len(result); i++ {
 		if result[i] < 0 {
-			return false
+			counter++
 		}
 	}
-	return true
+	if counter == len(result) {
+		return true
+	}
+	for i := 0; i < len(result); i++ {
+		if result[i] > 0 {
+			counter++
+		}
+	}
+	if counter == len(result) {
+		return true
+	}
+	return false
 }

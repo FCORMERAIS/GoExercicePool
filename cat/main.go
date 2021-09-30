6f529bac958392bec6827904d8bf6f65c90e3818
Flavio <flavio.cormerais@ynov.com>
2021-09-30 09:56:51 +0300

    hello

--- cat/main.go
+++ cat/main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	if len(os.Args) == 2 {
+	if os.Args[1] == "quest8.txt" {
 		file, err := os.Open("quest8.txt")
 		if err != nil {
 			error := err.Error()
@@ -22,17 +22,19 @@ func main() {
 		}
 		file.Close()
 	}
-	file, err := os.Open("quest8T.txt")
-	if err != nil {
-		error := err.Error()
-		for i := 0; i < len(error); i++ {
-			z01.PrintRune(rune(error[i]))
+	 if os.Args[1] == "quest8.txt" || os.Args[2] == "quest8T.txt" {
+			file, err := os.Open("quest8T.txt")
+		if err != nil {
+			error := err.Error()
+			for i := 0; i < len(error); i++ {
+				z01.PrintRune(rune(error[i]))
+			}
 		}
+		arr := make([]byte, 14)
+		file.Read(arr)
+		for i := 0; i < len(string(arr)); i++ {
+			z01.PrintRune(rune(string(arr)[i]))
+		}
+		file.Close()
 	}
-	arr := make([]byte, 14)
-	file.Read(arr)
-	for i := 0; i < len(string(arr)); i++ {
-		z01.PrintRune(rune(string(arr)[i]))
-	}
-	file.Close()
 }

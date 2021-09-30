415a85bb324ff9e36edf41d6500fd2ffb4734237
Flavio <flavio.cormerais@ynov.com>
2021-09-30 14:45:26 +0300

    hello

--- cat/main.go
+++ cat/main.go
@@ -1,87 +1,35 @@
 package main
 
 import (
-	"os"
+    "io/ioutil"
+    "os"
 
-	"github.com/01-edu/z01"
+    "github.com/01-edu/z01"
 )
 
+func PrintStr(s string) {
+    for i := 0; i < len(s); i++ {
+        z01.PrintRune(rune(s[i]))
+    }
+}
+
 func main() {
-	if len(os.Args) == 3 {
-		file, err := os.Open("quest8.txt")
-		if err != nil {
-			error := err.Error()
-			for i := 0; i < len(error); i++ {
-				z01.PrintRune(rune(error[i]))
-			}
-		}
-		if os.Args[1] != "quest8.txt" {
-			str := "ERROR: open abc: no such file or directory"
-			for i := 0; i < len(str); i++ {
-				z01.PrintRune(rune(str[i]))
-			}
-			z01.PrintRune(rune('\n'))
-			os.Exit(1)
-		}
-		arr := make([]byte, 14)
-		file.Read(arr)
-		for i := 0; i < len(string(arr)); i++ {
-			z01.PrintRune(rune(string(arr)[i]))
-		}
-		file.Close()
-		file, err = os.Open("quest8T.txt")
-		if os.Args[2] != "quest8T.txt" {
-			str := "ERROR: open abc: no such file or directory"
-			for i := 0; i < len(str); i++ {
-				z01.PrintRune(rune(str[i]))
-			}
-			z01.PrintRune(rune('\n'))
-			os.Exit(1)
-		}
-		arr = make([]byte, 14)
-		file.Read(arr)
-		for i := 0; i < len(string(arr)); i++ {
-			z01.PrintRune(rune(string(arr)[i]))
-		}
-		file.Close()
-	} else if len(os.Args) == 2 {
-		if os.Args[1] == "quest8.txt" {
-			file, err := os.Open("quest8.txt")
-			if err != nil {
-				error := err.Error()
-				for i := 0; i < len(error); i++ {
-					z01.PrintRune(rune(error[i]))
-				}
-			}
-			arr := make([]byte, 14)
-			file.Read(arr)
-			for i := 0; i < len(string(arr)); i++ {
-				z01.PrintRune(rune(string(arr)[i]))
-			}
-			file.Close()
-		}
-		if os.Args[1] == "quest8T.txt" {
-			file, err := os.Open("quest8T.txt")
-			if err != nil {
-				error := err.Error()
-				for i := 0; i < len(error); i++ {
-					z01.PrintRune(rune(error[i]))
-				}
-			}
-			arr := make([]byte, 14)
-			file.Read(arr)
-			for i := 0; i < len(string(arr)); i++ {
-				z01.PrintRune(rune(string(arr)[i]))
-			}
-			file.Close()
-		}
-		if os.Args[1] != "quest8.txt" && os.Args[1] != "quest8T.txt" {
-			str := "ERROR: open asd: no such file or directory"
-			for i := 0; i < len(str); i++ {
-				z01.PrintRune(rune(str[i]))
-			}
-			z01.PrintRune(rune('\n'))
-			os.Exit(1)
-		}
-	}
+    for i := 1; i < len(os.Args); i++ {
+        data, err := ioutil.ReadFile(os.Args[i])
+        if err != nil {
+            PrintStr("ERROR: ")
+            PrintStr(string(err.Error()))
+            PrintStr("\n")
+            os.Exit(1)
+        }
+        PrintStr(string(data))
+    }
+    if len(os.Args) == 1 {
+        data, err := ioutil.ReadAll(os.Stdin)
+        if err == nil {
+            PrintStr(string(data))
+        } else {
+            PrintStr(err.Error())
+        }
+    }
 }

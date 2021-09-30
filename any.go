fe93f6810ff052d4408174be9cde89e2c3285cc5
Flavio <flavio.cormerais@ynov.com>
2021-09-30 12:54:07 +0300

    hello

---
+++ any.go
@@ -0,0 +1,14 @@
+package piscine 
+
+func Any(f func(string) bool, a []string) bool {
+	var result []bool
+	for i := 0; i < len(a); i++ {
+		result = append(result, f(a[i]))
+	}
+	for i := 0; i < len(result); i++ {
+		if result[i] == true {
+			return true
+		}
+	}
+	return false
+}

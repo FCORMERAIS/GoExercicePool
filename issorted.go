7a5d3250660ff4595bf2c2f117d5fd19dd0277e1
Flavio <flavio.cormerais@ynov.com>
2021-09-30 14:21:25 +0300

    hello

--- issorted.go
+++ issorted.go
@@ -6,7 +6,7 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 		result = append(result, f(a[i], a[i+1]))
 	}
 	for i := 0; i < len(result); i++ {
-		if result[i] < 0 {
+		if result[i] > 0 {
 			return false
 		}
 	}

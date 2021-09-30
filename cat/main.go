1d2731a434d8d65eb0f83cab5b9edafb27b5623c
Flavio <flavio.cormerais@ynov.com>
2021-09-30 10:08:38 +0300

    hello

--- cat/main.go
+++ cat/main.go
@@ -7,34 +7,65 @@ import (
 )
 
 func main() {
-	if os.Args[1] == "quest8.txt" {
-		file, err := os.Open("quest8.txt")
-		if err != nil {
-			error := err.Error()
-			for i := 0; i < len(error); i++ {
-				z01.PrintRune(rune(error[i]))
+	if len(os.Args) == 2 {
+		if os.Args[1] == "quest8.txt" && os.Args[2] == "quest8T.txt" {
+			file, err := os.Open("quest8.txt")
+			if err != nil {
+				error := err.Error()
+				for i := 0; i < len(error); i++ {
+					z01.PrintRune(rune(error[i]))
+				}
 			}
-		}
-		arr := make([]byte, 14)
-		file.Read(arr)
-		for i := 0; i < len(string(arr)); i++ {
-			z01.PrintRune(rune(string(arr)[i]))
-		}
-		file.Close()
-	}
-	if os.Args[1] == "quest8T.txt" {
-		file, err := os.Open("quest8T.txt")
-		if err != nil {
-			error := err.Error()
-			for i := 0; i < len(error); i++ {
-				z01.PrintRune(rune(error[i]))
+			arr := make([]byte, 14)
+			file.Read(arr)
+			for i := 0; i < len(string(arr)); i++ {
+				z01.PrintRune(rune(string(arr)[i]))
 			}
+			file.Close()
+			file, err = os.Open("quest8T.txt")
+			if err != nil {
+				error := err.Error()
+				for i := 0; i < len(error); i++ {
+					z01.PrintRune(rune(error[i]))
+				}
+			}
+			arr = make([]byte, 14)
+			file.Read(arr)
+			for i := 0; i < len(string(arr)); i++ {
+				z01.PrintRune(rune(string(arr)[i]))
+			}
+			file.Close()
 		}
-		arr := make([]byte, 14)
-		file.Read(arr)
-		for i := 0; i < len(string(arr)); i++ {
-			z01.PrintRune(rune(string(arr)[i]))
+	} else {
+		if os.Args[1] == "quest8.txt" {
+			file, err := os.Open("quest8.txt")
+			if err != nil {
+				error := err.Error()
+				for i := 0; i < len(error); i++ {
+					z01.PrintRune(rune(error[i]))
+				}
+			}
+			arr := make([]byte, 14)
+			file.Read(arr)
+			for i := 0; i < len(string(arr)); i++ {
+				z01.PrintRune(rune(string(arr)[i]))
+			}
+			file.Close()
 		}
-		file.Close()
+		if os.Args[1] == "quest8T.txt" {
+			file, err := os.Open("quest8T.txt")
+			if err != nil {
+				error := err.Error()
+				for i := 0; i < len(error); i++ {
+					z01.PrintRune(rune(error[i]))
+				}
+			}
+			arr := make([]byte, 14)
+			file.Read(arr)
+			for i := 0; i < len(string(arr)); i++ {
+				z01.PrintRune(rune(string(arr)[i]))
+			}
+			file.Close()
+		}
 	}
 }

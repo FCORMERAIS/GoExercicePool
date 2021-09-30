f57ab7c5538e254817c9598275c1c1fa241f22a7
Flavio <flavio.cormerais@ynov.com>
2021-09-30 09:23:44 +0300

    hello

---
+++ cat/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 2 {
+		file, err := os.Open("quest8.txt")
+		if err != nil {
+			fmt.Printf(err.Error())
+		}
+		arr := make([]byte, 14)
+		file.Read(arr)
+		fmt.Println(string(arr))
+		file.Close()
+	} else if len(os.Args) == 1 {
+		fmt.Println("File name missing")
+	} else {
+		fmt.Println("Too many arguments")
+	}
+}

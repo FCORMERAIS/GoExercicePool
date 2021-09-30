66babd13f71114b731f7ab44f3ecfa691a177e69
Flavio <flavio.cormerais@ynov.com>
2021-09-30 14:02:12 +0300

    hello

--- any.go
+++ any.go
@@ -1,14 +1,10 @@
 package piscine
 
 func Any(f func(string) bool, a []string) bool {
-	var result []bool
-	for i := 0; i < len(a); i++ {
-		result = append(result, f(a[i]))
-	}
-	for i := 0; i < len(result); i++ {
-		if result[i] == true {
-			return true
-		}
-	}
-	return false
+    for i := 0; i < len(a); i++ {
+        if f(a[i]) {
+            return true
+        }
+    }
+    return false
 }

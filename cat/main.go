62b66995d05474ef51ec2dd8d1a0db14beed0a53
Flavio <flavio.cormerais@ynov.com>
2021-09-30 11:24:47 +0300

    hello

--- cat/main.go
+++ cat/main.go
@@ -69,14 +69,11 @@ func main() {
 		}
 		if os.Args[1] != "quest8.txt" && os.Args[1] != "quest8T.txt" {
 			str := "ERROR: open asd: no such file or directory"
-			str2 := "exit status 1"
 			for i := 0; i < len(str); i++ {
 				z01.PrintRune(rune(str[i]))
 			}
 			z01.PrintRune(rune('\n'))
-			for i := 0; i < len(str2); i++ {
-				z01.PrintRune(rune(str2[i]))
-			}
+			os.Exit(1)
 		}
 	}
 }

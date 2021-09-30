5e5ef3cc32e2567c8caf62952aad4f23c666e0eb
Flavio <flavio.cormerais@ynov.com>
2021-09-30 09:57:17 +0300

    hello

--- cat/main.go
+++ cat/main.go
@@ -22,8 +22,8 @@ func main() {
 		}
 		file.Close()
 	}
-	 if os.Args[1] == "quest8.txt" || os.Args[2] == "quest8T.txt" {
-			file, err := os.Open("quest8T.txt")
+	if os.Args[1] == "quest8.txt" || os.Args[2] == "quest8T.txt" {
+		file, err := os.Open("quest8T.txt")
 		if err != nil {
 			error := err.Error()
 			for i := 0; i < len(error); i++ {

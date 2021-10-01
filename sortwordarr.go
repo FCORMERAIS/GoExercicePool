c66701d986dcab027dc160bc824e966650daf5b5
Flavio <flavio.cormerais@ynov.com>
2021-10-01 11:39:29 +0300

    hello

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,31 +1,15 @@
 package piscine
 
-import (
-	"os"
-)
-
-func SortIntegerTabl(table []string) []string {
-	index := 0
-	if len(table) == 1 {
-		return table
-	}
-	for i := 0; i <= len(table)-1; i++ {
-		minimum := table[i]
-		for k := i; k < len(table); k++ {
-			if table[k] <= minimum {
-				minimum = table[k]
-				index = k
-			}
-		}
-		table[i], table[index] = table[index], table[i]
-	}
-	return table
+func SortWordArr(table []string) {
+    for min_index := 0; min_index < len(table)-1; min_index++ {
+        for index := min_index + 1; index < len(table); index++ {
+            if table[index] < table[min_index] {
+                Swap2(&table[index], &table[min_index])
+            }
+        }
+    }
 }
 
-func SortWordArr(a []string) {
-	sentence := SortIntegerTabl(a)
-	for i := 0; i < len(sentence)-1; i++ {
-		os.Stderr.WriteString(sentence[i])
-	}
-	os.Stderr.WriteString("\n")
+func Swap2(a *string, b *string) {
+    *a, *b = *b, *a
 }

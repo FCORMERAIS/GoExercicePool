763d649cdac8c48efff9507528bec14aae933b8a
Flavio <flavio.cormerais@ynov.com>
2021-10-01 11:41:25 +0300

    hello

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,15 +1,15 @@
 package piscine
 
 func SortWordArr(table []string) {
-    for min_index := 0; min_index < len(table)-1; min_index++ {
-        for index := min_index + 1; index < len(table); index++ {
-            if table[index] < table[min_index] {
-                Swap2(&table[index], &table[min_index])
-            }
-        }
-    }
+	for min_index := 0; min_index < len(table)-1; min_index++ {
+		for index := min_index + 1; index < len(table); index++ {
+			if table[index] < table[min_index] {
+				Swap2(&table[index], &table[min_index])
+			}
+		}
+	}
 }
 
 func Swap2(a *string, b *string) {
-    *a, *b = *b, *a
+	*a, *b = *b, *a
 }

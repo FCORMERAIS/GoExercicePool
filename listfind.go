687d141c05f528323fec7fe76520fa717d6feb76
Flavio <flavio.cormerais@ynov.com>
2021-10-04 17:01:28 +0300

    hello

---
+++ listfind.go
@@ -0,0 +1,16 @@
+package piscine
+
+func CompStr(a, b interface{}) bool {
+	return a == b
+}
+
+func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
+	var tmp *NodeL = l.Head
+	for tmp != nil {
+		if comp(tmp, ref) == true {
+			return &tmp.Data
+		}
+		tmp = tmp.Next
+	}
+	return nil
+}

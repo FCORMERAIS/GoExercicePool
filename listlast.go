89ba87d90ff3dbe4b71a3f7ff344997d41204e0f
Flavio <flavio.cormerais@ynov.com>
2021-10-04 14:46:14 +0300

    hello

--- listlast.go
+++ listlast.go
@@ -1,11 +1,12 @@
 package piscine
 
 func ListLast(l *List) interface{} {
-	if l.Head == nil {
+	tmp := l
+	if tmp.Head == nil {
 		return nil
 	}
-	for l.Head.Next != nil {
-		l.Head = l.Head.Next
+	for tmp.Head.Next != nil {
+		tmp.Head = tmp.Head.Next
 	}
-	return l.Head.Data
+	return tmp.Head.Data
 }

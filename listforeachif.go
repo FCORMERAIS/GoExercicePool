15d7eddb922bf57899660a3cc291962a70463beb
Flavio <flavio.cormerais@ynov.com>
2021-10-04 16:48:07 +0300

    hello

---
+++ listforeachif.go
@@ -0,0 +1,29 @@
+package piscine
+
+func IsPositiveNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return node.Data.(int) > 0
+	default:
+		return false
+	}
+}
+
+func IsAlNode(node *NodeL) bool {
+	switch node.Data.(type) {
+	case int, float32, float64, byte:
+		return false
+	default:
+		return true
+	}
+}
+
+func ListForEachIf(l *List, f func(*NodeL), cond func(*NodeL) bool) {
+	var tmp *NodeL = l.Head
+	for tmp != nil {
+		if cond(tmp) == true {
+			f(tmp)
+		}
+		tmp = tmp.Next
+	}
+}

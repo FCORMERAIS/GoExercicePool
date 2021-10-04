97d48f96cd2b48547a2759df73a6c0900aec84a7
Flavio <flavio.cormerais@ynov.com>
2021-10-04 14:14:59 +0300

    hello

--- listreverse.go
+++ listreverse.go
@@ -1,7 +1,7 @@
 package piscine
 
 func ListReverse(l *List) {
-	unMaillon := &List{Head: nil}
+	unMaillon := &List{}
 	for i := 0; i < ListSize(l); i++ {
 		ListPushFront(unMaillon, removeLast(l))
 	}
@@ -9,10 +9,10 @@ func ListReverse(l *List) {
 }
 
 func removeLast(l *List) interface{} {
-	for l.Head != nil {
-		l.Head = l.Head.Next.Next
+	for l.Head.Next != nil {
+		l.Head = l.Head.Next
 	}
-	result := l.Head.Next
+	result := l.Head
 	l.Tail = nil
 	return result
 }

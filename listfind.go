030d32afbb8885ad12953fcea5bf64413f238b0e
Flavio <flavio.cormerais@ynov.com>
2021-10-04 17:03:05 +0300

    hello

--- listfind.go
+++ listfind.go
@@ -6,11 +6,14 @@ func CompStr(a, b interface{}) bool {
 
 func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
 	var tmp *NodeL = l.Head
+	if tmp.Data == nil {
+		return nil
+	}
 	for tmp != nil {
 		if comp(tmp, ref) == true {
 			return &tmp.Data
 		}
 		tmp = tmp.Next
 	}
-	return &tmp.Data
+	return nil
 }

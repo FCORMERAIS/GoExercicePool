112cf284cecaa1d4349d1b8ea9ea765d6cdbeb56
Flavio <flavio.cormerais@ynov.com>
2021-10-05 12:13:14 +0300

    hello

--- listmerge.go
+++ listmerge.go
@@ -3,9 +3,14 @@ package piscine
 func ListMerge(l1 *List, l2 *List) {
 	if l1 == nil {
 		l1 = l2
-	} else if l2 == nil {
+	} else if l2 == nil{
 		l2 = l1
 	} else {
 		l1.Tail.Next = l2.Head
+		tmp := l1
+		for l1.Head.Next != nil {
+			tmp.Head = tmp.Head.Next
+		}
+		l1.Tail = tmp.Head
 	}
 }

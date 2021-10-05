7a36c62bf2b4ae855a14dc363c24075f9d866baf
Flavio <flavio.cormerais@ynov.com>
2021-10-05 15:43:46 +0300

    hello

--- listsort.go
+++ listsort.go
@@ -9,15 +9,16 @@ func ListSort(l *NodeI) *NodeI {
 	tmp := l
 	if tmp == nil {
 		return l
-	}
-	for i := 0; i < 45; i++ {
-		for tmp.Next != nil {
-			if tmp.Data > tmp.Next.Data {
-				tmp.Data, tmp.Next.Data = tmp.Next.Data, tmp.Data
+	} else {
+		for i := 0; i < 45; i++ {
+			for tmp.Next != nil {
+				if tmp.Data > tmp.Next.Data {
+					tmp.Data, tmp.Next.Data = tmp.Next.Data, tmp.Data
+				}
+				tmp.Data = tmp.Next.Data
 			}
-			tmp.Data = tmp.Next.Data
+			tmp = l
 		}
-		tmp = l
 	}
 	return l
 }
